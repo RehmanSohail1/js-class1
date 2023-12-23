ebe173d846bb868df61c70cbecd29271ebcf4f48
RehmanSohail1 <111071020+RehmanSohail1@users.noreply.github.com>
2023-12-23 05:19:18 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,11 @@
+var a = +prompt("pys");
+var b = +prompt("math");
+var c = +prompt("eng");
+var d = +prompt("bio");
+var e = +prompt("urdu");
+
+var f = 500;
+
+var percentage = ((a+b+c+d+e)/f)*100;
+
+alert (percentage);
